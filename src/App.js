0331df5cf97a6bd68090c49970f8a675ba195699
Divit Karnawat <divitkarnawat1@gmail.com>
2020-06-24 19:40:35 +0530

    working on react lazy, suspense

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React , { Suspense, lazy}from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Header from './components/Header/Header.js';
-import Footer from './components/Footer/Footer.js';
-import HomePage from './views/homepage';
-import BlogPage from './views/blogpage';
-import PrivacyPolicy from './views/documents/PrivacyPolicy';
-import TnC from './views/documents/TnC';
+import Loader from './components/Loader';
+const Footer = lazy(()=> import('./components/Footer/Footer.js'));
+const BlogPage = lazy(()=>import('./views/blogpage'));
+const PrivacyPolicy = lazy(()=>import('./views/documents/PrivacyPolicy'));
+const TnC = lazy(()=>import('./views/documents/TnC'));
+const HomePage = lazy(() => import('./views/homepage'));
+
 function App() {
   return (
   <Router>
   <Header />
+  <Suspense fallback={<Loader />}>
   <Switch>
     <Route exact path = "/" component = {HomePage} />
     <Route path = "/blog/:author/:blogid" component = {BlogPage} />
     <Route path = "/privacy-policy/" component = {PrivacyPolicy} />
     <Route path = "/terms-and-conditions/" component = {TnC} />
   </Switch>
+  
+  
   <Footer />
+  </Suspense>
   </Router>    
   );
 }
