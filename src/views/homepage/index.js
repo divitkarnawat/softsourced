0c54d3ff4e135b2799039339bc6dd31a7ee46642
Divit Karnawat <divitkarnawat1@gmail.com>
2020-07-12 13:37:04 +0530

    pagespeed codesplit8

--- src/views/homepage/index.js
+++ src/views/homepage/index.js
@@ -67,11 +67,16 @@ class HomePage extends Component  {
             <div id = {this.props.t('common:nav.0')}><About/></div>
         
             <Product change_p_slide_id = {this.props.change_p_slide_id} />
+            {
+                (1==2) ? <>
+            
             <div id = {this.props.t('common:nav.2')}><Projects /></div> 
             <WhyUs/>
             <div id = {this.props.t('common:nav.3')}><Team /></div>
             <div id = {this.props.t('common:nav.1')}><Pricing support_catUpdate = {this.support_catUpdate.bind(this)} p_slide_id = {this.props.p_slide_id} /></div>
             <div id ={this.props.t('common:nav.4')}><Blog/></div>
+            </> : ""
+    }
             <div id = {this.props.t('common:nav.5')}><Contact propid = {this.state.contact_ran} email={this.state.email} support={this.state.support} support_cat_id = {this.state.support_cat_id} support_id = {this.state.support_id} /></div>
             </Suspense>
   </>
